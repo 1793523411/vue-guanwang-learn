30c78c05732659025e8a596f24b7751c80f79d17
ygjzs <1793523411@qq.com>
2020-08-24 22:55:54 +0800

    first

---
+++ gloab-regis-component/main.js
@@ -0,0 +1,26 @@
+import Vue from'vue'
+import upperFirst from 'lodash/upperFirst'
+import camelCase from 'lodash/camelCase'
+
+const requireComponent = require.context(
+    './components',
+    //不查询子目录
+    false,
+    /Base[A-Z]\w+\.(vue|js)$/
+)
+
+requireComponent.keys().forEach(fileName => {
+    const componentConfig = requireComponent(fileName) 
+    const componentName = upperFirst(
+        camelCase(
+            fileName
+            .split('/')
+            .pop()
+            .replace(/\.\w+$/,'')
+        )
+    )
+        Vue.component(
+            componentName,
+            componentConfig.default || componentConfig
+        )
+})
